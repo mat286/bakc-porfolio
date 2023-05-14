d0eb3c6491a8fb1cc3c3cb9b3d986257fbbe7b6e
mat286 <riveramateo10432@gmail.com>
2023-05-14 11:03:58 -0300

    5.0

--- src/main/java/com/SpringBoot/Portofolio/controller/ConocimientoController.java
+++ src/main/java/com/SpringBoot/Portofolio/controller/ConocimientoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://porfolio-784e6.web.app")
+@CrossOrigin(origins = "https://porfolio-784e6.web.app/Conocimientos")
 public class ConocimientoController {
     
     @Autowired
--- src/main/java/com/SpringBoot/Portofolio/controller/EducacionController.java
+++ src/main/java/com/SpringBoot/Portofolio/controller/EducacionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "https://porfolio-784e6.web.app")
+@CrossOrigin(origins = "https://porfolio-784e6.web.app/Educaciones")
 public class EducacionController {
     
     @Autowired
